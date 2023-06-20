b2a89577cbf96f2e47bac315f4e475e0563c3d45
haddow <sam@samhaddow.com>
2023-06-21 07:21:37 +0930

    fixed time zone to be local based on lat lon added requirements.txt

--- srt-gpx.py
+++ srt-gpx.py
@@ -2,54 +2,49 @@ import os
 import re
 import gpxpy.gpx
 from datetime import datetime
+from timezonefinder import TimezoneFinder
+import pytz
 
-# Regular expression pattern to match the GPS info
+# regex pattern for GPS info
 pattern = re.compile(r'\[(\w+): ([\-\d\.]+)\]')
 
-# Regular expression pattern to match the altitude info
+# regex pattern for altitude info
 alt_pattern = re.compile(r'\[rel_alt: [\-\d\.]+ abs_alt: ([\-\d\.]+)\]')
 
-
-# Directory paths
 input_dir = 'input'
 output_dir = 'output'
 
-# Ensure output directory exists
 os.makedirs(output_dir, exist_ok=True)
 
-# Loop over all files in the input directory
 for filename in os.listdir(input_dir):
-    # Check if the file has an SRT extension (ignoring case)
     if filename.lower().endswith('.srt'):
-        # Open the SRT file
         with open(os.path.join(input_dir, filename), 'r') as f:
             content = f.read()
 
-        # Create a new GPX file
         gpx = gpxpy.gpx.GPX()
 
-        # Create a new GPX track
         gpx_track = gpxpy.gpx.GPXTrack()
         gpx.tracks.append(gpx_track)
 
-        # Create a new GPX segment
         gpx_segment = gpxpy.gpx.GPXTrackSegment()
         gpx_track.segments.append(gpx_segment)
 
-        # Split the content into blocks
+        tf = TimezoneFinder()
+
+        local_tz = None
+
         blocks = content.split('\n\n')
 
-        # Loop over the blocks
         for block in blocks:
             lines = block.split('\n')
             if len(lines) >= 5:
                 # Parse the timestamp from the fourth line
-                timestamp = datetime.strptime(lines[3].strip(), "%Y-%m-%d %H:%M:%S.%f")
+                timestamp_str = lines[3].strip()
 
                 # Parse the GPS info from the fifth line
                 gps_info = lines[4]
 
-                # Use a regular expression to extract the latitude and longitude
+                # extract the latitude and longitude
                 data = dict(pattern.findall(gps_info))
 
                 if 'latitude' in data and 'longitude' in data:
@@ -62,10 +57,20 @@ for filename in os.listdir(input_dir):
                     else:
                         alt = None  # if no absolute altitude data is available, set it to None
 
-                    # Create a new GPX point and add it to the segment
+                    if local_tz is None:
+                        # Get timezone string for the given latitude and longitude
+                        timezone_str = tf.timezone_at(lng=lon, lat=lat)
+                        if timezone_str:
+                            local_tz = pytz.timezone(timezone_str)
+
+                    if local_tz:
+                        timestamp = datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S.%f")
+                        timestamp = local_tz.localize(timestamp)
+                    else:
+                        timestamp = datetime.strptime(timestamp_str, "%Y-%m-%d %H:%M:%S.%f")
+
                     gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(lat, lon, elevation=alt, time=timestamp))
 
-        # Write the GPX file
         output_filename = os.path.splitext(filename)[0] + '.gpx'
         with open(os.path.join(output_dir, output_filename), 'w') as f:
             f.write(gpx.to_xml())
